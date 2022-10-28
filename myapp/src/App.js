a7476bf5d75630b813adb8f567f78f03a182471e
Shrihari Raut <101625830+harry131313@users.noreply.github.com>
2022-10-28 23:31:23 +0530

    React UI

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import { Navbar, Feed, VedioDetail, ChannelDetail, SearchFeed } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, My First Repository on git</h1>
-    </div>
+
+const App = () =>  (
+
+    <BrowserRouter>
+      <Box sx= {{ backgroundColor: '#000'}}>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/vedio/:id' exact element={<VedioDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail/>} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+
+        </Routes>
+
+      </Box>
+    </BrowserRouter>
   );
-}
 
-export default App;
+
+export default App
